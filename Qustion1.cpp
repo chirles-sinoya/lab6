4968080dc37d2a282dbb25b07b7273ac188df896
Chirles Sinoya <bsc-inf-25-22@unima.ac.mw>
2024-04-26 14:49:45 +0200

    add lab6

--- Qustion1.cpp
+++ Qustion1.cpp
@@ -1,26 +1,27 @@
 #include <iostream>
 #include <string>
+using namespace std;
+
 
 int main() {
-    // Dynamically allocate an integer
+    //  allocate an integer dynamically
     int* dynamicInt = new int;
 
-    // Dynamically allocate a string
-    std::string* dynamicString = new std::string;
+    
+    string* dynamicString = new std::string;
 
     // Prompt user for input
-    std::cout << "Enter an integer value: ";
-    std::cin >> *dynamicInt;
+    cout << "Enter an integer value: ";
+    cin >> *value;
 
-    std::cout << "Enter a string value: ";
-    std::cin.ignore(); // Ignore newline character left in the buffer
-    std::getline(std::cin, *dynamicString);
+    cout << "Enter a string : ";
+    cin.delete();  
+    getline(std::cin, *dynamicString);
 
-    // Output the dynamically allocated values
-    std::cout << "Dynamically allocated integer value: " << *dynamicInt << std::endl;
-    std::cout << "Dynamically allocated string value: " << *dynamicString << std::endl;
+    cout << "Dynamically allocated integer value: " << *dynamicInt << std::endl;
+    cout << "Dynamically allocated string value: " << *dynamicString << std::endl;
 
-    // Clean up memory
+    // Clear used mwmory
     delete dynamicInt;
     delete dynamicString;
 
